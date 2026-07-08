31cc5aca61a3691ea17d15e6bd5f013180bf88c9
TaranaKhanna <taranakhanna5@gmail.com>
2026-07-08 09:12:21 +0000

    feat(producer): implement job creation and retrieval with Redis

--- producer/src/index.ts
+++ producer/src/index.ts
@@ -1,11 +1,14 @@
+import "dotenv/config";
 import express from "express";
-import dotenv from "dotenv";
+import jobsRouter from "./routes/jobs.js";
 
-dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use("/jobs", jobsRouter);
 
-app.listen(process.env.PORT, () => {
-    console.log("Producer is running!!");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
 });
